b1a7b6202d1f94b4e011806b94bd66424eb814c1
JacqueRobles <valeria.robles.tesillo@gmail.com>
2023-12-24 02:19:40 -0400

    Add RolSeeder for database seeding

---
+++ database/seeders/RolSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+class RolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $role1 = Role::create(['name' => 'Directive']);
+        $role2 = Role::create(['name' => 'Director']);
+        $role3 = Role::create(['name' => 'Pupil']);
+        $role4 = Role::create(['name' => 'Partner']);
+        $role5 = Role::create(['name' => 'Player']);
+
+
+    }
+}
