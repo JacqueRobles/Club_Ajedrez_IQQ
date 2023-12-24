bf8c5934e8ca4f8938df3db021532b993b7330d1
JacqueRobles <valeria.robles.tesillo@gmail.com>
2023-12-24 01:40:44 -0400

    Add new models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -17,6 +18,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -27,6 +29,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'surname',
+        'rut',
+        'v_digit',
+        'phone',
+        'state',
     ];
 
     /**
