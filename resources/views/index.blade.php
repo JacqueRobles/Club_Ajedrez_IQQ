629a9a14c798db645177a491d6fc86d483e22a70
JacqueRobles <valeria.robles.tesillo@gmail.com>
2023-12-25 15:05:53 -0400

    Add dashboard link for directive users

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,7 +38,9 @@
           @if (Route::has('login'))
             <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                 @auth
+                  @if(Auth::user()->directive && Auth::user()->directive->role == 'directive')
                     <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                  @endif
                 @else
                     <a href="{{ route('login') }}" class="nav-link active">Log in</a>
 
