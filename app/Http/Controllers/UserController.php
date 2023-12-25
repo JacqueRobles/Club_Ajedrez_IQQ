ea9304e7907b0f8397752f48b18eab3523fa685d
JacqueRobles <valeria.robles.tesillo@gmail.com>
2023-12-25 17:18:47 -0400

    Refactor user creation logic and add role assignment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,84 +22,38 @@ class UserController extends Controller
         $this->userService = $userService;
     }
 
-    public function store(Request $request)
+    public function registerPlayer(Request $request)
     {
         // Validate the request data
         $request->validate([
             // Add your validation rules here
         ]);
-
-        // Determine the type of user being created
-        switch ($request->input('user_type')) {
-            case 'player':
-                $userable = Player::create($request->only([
-                    // List the player attributes here
-                ]));
-                break;
-            default:
-                // Check if the authenticated user is a directive
-                if (!(Auth::user()->userable instanceof Directive)) {
-                    // Return an error response
-                    return response()->json(['error' => 'Unauthorized'], 403);
-                }
-
-                switch ($request->input('user_type')) {
-                    case 'directive':
-                        $userable = Directive::create($request->only([
-                            // List the directive attributes here
-                        ]));
-                        break;
-                    case 'partner':
-                        $userable = Partner::create($request->only([
-                            // List the partner attributes here
-                        ]));
-                        break;
-                    case 'director':
-                        $userable = Director::create($request->only([
-                            // List the director attributes here
-                        ]));
-                        break;
-                    case 'pupil':
-                        $userable = Pupil::create($request->only([
-                            // List the pupil attributes here
-                        ]));
-                        break;
-                    default:
-                        // Handle invalid user type
-                        break;
-                }
-                break;
-    }
-
-    // Create the User
-    $user = User::create(array_merge($request->only([
-        // List the user attributes here
-    ]), [
-        'userable_id' => $userable->id,
-        'userable_type' => get_class($userable)
-    ]));
-        // Redirect or return a response
+    
+        // Create the User
+        $user = $this->userService->createUser($request->all(), 'player');
+    
         // Return the created User
         return response()->json($user, 201);
-
-        // Redirect the user to the dashboard
-        // return redirect()->route('dashboard');
     }
-
-    public function createDirectorUser(Request $request)
+    
+    public function store(Request $request)
     {
-        $attributes = $request->all();
-        $user = $this->userService->createDirectorUser($attributes);
-
-        // Additional logic
-        
+        // Check if the authenticated user is a directive
+        if (!(Auth::user()->userable instanceof Directive)) {
+            // Return an error response
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+    
+        // Validate the request data
+        $request->validate([
+            // Add your validation rules here
+        ]);
+    
+        // Create the User
+        $user = $this->userService->createUser($request->all(), $request->input('user_type'));
+    
+        // Return the created User
+        return response()->json($user, 201);
     }
 
-    public function createPartnerUser(Request $request)
-    {
-        $attributes = $request->all();
-        $user = $this->userService->createPartnerUser($attributes);
-
-        // Additional logic
-    }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,22 +2,53 @@
 namespace App\Services;
 
 use App\Models\User;
+use App\Models\Directive;
+use App\Models\Partner;
+use App\Models\Director;
+use App\Models\Pupil;
+use App\Models\Player;
+use Spatie\Permission\Models\Role;
 
 class UserService
 {
-    public function createDirectorUser($attributes)
+    public function createUser(array $attributes, string $userType)
     {
-        $user = User::create($attributes);
-        $user->assignRole('director');
+        // Determine the type of user being created
+        switch ($userType) {
+            case 'directive':
+                $userable = Directive::create($attributes);
+                break;
+            case 'partner':
+                $userable = Partner::create($attributes);
+                break;
+            case 'director':
+                $userable = Director::create($attributes);
+                break;
+            case 'pupil':
+                $userable = Pupil::create($attributes);
+                break;
+            case 'player':
+                $userable = Player::create($attributes);
+                break;
+            default:
+                // Handle invalid user type
+                break;
+        }
+
+        // Create the User
+        $user = User::create([
+            'userable_id' => $userable->id,
+            'userable_type' => get_class($userable),
+            // Add other user attributes here
+        ]);
+
+        // Assign the role to the user
+        $role = Role::where('name', $userType)->first();
+        if ($role) {
+            $user->assignRole($userType);
+        }
 
         return $user;
     }
 
-    public function createPartnerUser($attributes)
-    {
-        $user = User::create($attributes);
-        $user->assignRole('partner');
-
-        return $user;
-    }
 }
