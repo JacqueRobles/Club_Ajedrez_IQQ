2951256dfa6c5e7debfec08be9cbe9a9fd3b5694
JacqueRobles <valeria.robles.tesillo@gmail.com>
2023-12-25 14:58:05 -0400

    Remove redundant code and update navigation bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,20 +12,8 @@
 </head>
 
 <body>
-  @if (Route::has('login'))
-      <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-          @auth
-              <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-          @else
-              <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-              @if (Route::has('register'))
-                  <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-              @endif
-          @endauth
-      </div>
-  @endif
   <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #f2f3f3;">
+    
     <div class="container-fluid">
       <img src="img/oficial.png" href="#" alt="" width="100" height="60" class="d-inline-block align-text-top">
       <h6>club de iquique</h6>
@@ -47,7 +35,19 @@
           </li>
         </li>
         <li class="nav-item">
-          
+          @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="nav-link active">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="nav-link active">Register</a>
+                    @endif
+                @endauth
+            </div>
+          @endif
         </li>
         </ul>
       </div>
