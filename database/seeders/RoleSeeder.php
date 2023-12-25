28173f0dbbe92e3caad97c48c8e53125749cabbc
JacqueRobles <valeria.robles.tesillo@gmail.com>
2023-12-25 14:51:59 -0400

    Add RoleSeeder and update index.blade.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use Spatie\Permission\Models\Role;
-class RolSeeder extends Seeder
+class RoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
